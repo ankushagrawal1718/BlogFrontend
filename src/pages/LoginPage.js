2a50627f1079d5565eebf0ed6570293abd7852f0
Ankush Agrawal <ankushagrawal4321@gmail.com>
2024-02-19 09:16:25 +0530

    removed slash in login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -13,7 +13,7 @@ const LoginPage = () => {
 
  async function login(ev){
     ev.preventDefault();
-    const response =  await fetch('/login',{
+    const response =  await fetch('login',{
       method:'POST',
       body: JSON.stringify({username,password}),
       headers: {'Content-Type':'application/json'},
