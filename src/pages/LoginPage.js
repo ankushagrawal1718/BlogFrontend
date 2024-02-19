6e5caf97444a3cd8b968cef31343e9af7ae27367
Ankush Agrawal <ankushagrawal4321@gmail.com>
2024-02-19 09:25:36 +0530

    changed to basic route

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -13,7 +13,7 @@ const LoginPage = () => {
 
  async function login(ev){
     ev.preventDefault();
-    const response =  await fetch('login',{
+    const response =  await fetch('https://ankushblog.vercel.app/login',{
       method:'POST',
       body: JSON.stringify({username,password}),
       headers: {'Content-Type':'application/json'},
