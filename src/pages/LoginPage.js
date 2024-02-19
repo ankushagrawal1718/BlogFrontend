3499e1b9aa3922962feed512047d1a3cf11db6b4
Ankush Agrawal <ankushagrawal4321@gmail.com>
2024-02-19 09:49:54 +0530

    added backend route in loginPage

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -13,7 +13,7 @@ const LoginPage = () => {
 
  async function login(ev){
     ev.preventDefault();
-    const response =  await fetch('https://ankushblog.vercel.app/login',{
+    const response =  await fetch('https://blog-rg2y.onrender.com/login',{
       method:'POST',
       body: JSON.stringify({username,password}),
       headers: {'Content-Type':'application/json'},
