1fd1f27f2115360614e006f2baf734897c14eb72
Ankush Agrawal <ankushagrawal4321@gmail.com>
2024-02-20 09:14:10 +0530

    afsdfas

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -21,7 +21,7 @@ const LoginPage = () => {
     })
     console.log(response);
     if(response.ok){
-        document.cookie = `token=${response.data.token}; path=/; secure; HttpOnly`;
+        document.cookie = `token=${response.token}; path=/; secure; HttpOnly`;
         response.json().then(userInfo=>{
           setUserInfo(userInfo);
           setRedirect(true);
